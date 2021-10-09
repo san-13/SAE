52a8283929ebe260b1f95d3e233ba47dc0c4ba25
Sandeep <sandeepvermaslg77@gmail.com>
2021-10-09 18:49:18 +0530

    initial commit

--- app/src/main/java/com/sv/sae/SplashScreen.kt
+++ app/src/main/java/com/sv/sae/SplashScreen.kt
@@ -3,12 +3,23 @@ package com.sv.sae
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_splash_screen)
-        startActivity(Intent(this,MainActivity::class.java))
-        finish()
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+
     }
 }
